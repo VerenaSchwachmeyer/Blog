3edb76b4663e0efe3e5fc07ea1ba1beb0f88571f
VerenaSchwachmeyer <Vronus@gmx.de>
2023-01-25 17:35:00 +0100

    added first js code

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 
 function changeButton(){
+    //change button after clicking on it
     const element = document.getElementById('newsletter-button');
-
     element.innerHTML="Alles klar!";
     element.style.backgroundColor='lightgrey';
     element.style.color='grey';
     element.setAttribute("disabled", true);
-
+    
+   // change mail input field
     document.getElementById('email').setAttribute("disabled", true);
 
 }
